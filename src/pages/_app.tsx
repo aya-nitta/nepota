4dde8367e730c3b7d516384d3c2eb8686523fa8e
soichiro-nitta <soic1226@gmail.com>
2020-05-23 00:32:00 +0900

    feat: app.tsxでgoogleフォントを読み込み

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,18 @@ import * as React from 'react'
 import { AppProps } from 'next/app'
 import Layout from '~/layouts/default'
 import { GlobalStyle } from '~/utils/styles'
+import Head from 'next/head'
 // import { Provider } from 'react-redux'
 // import store from '~/store'
 
 const _App: React.FC<AppProps> = props => (
   <>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
     <GlobalStyle />
     <Layout {...props} />
   </>
