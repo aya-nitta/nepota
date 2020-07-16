5bce11dc7ad558c057681b6977e59849b18c06d3
Aya Nitta <aya@AyanoMacBook.local>
2020-07-16 22:31:03 +0900

    文章を追加しました

--- src/pages/nepota/index.tsx
+++ src/pages/nepota/index.tsx
@@ -7,20 +7,79 @@ type ComponentProps = { className: string } & ContainerProps
 
 const Component: React.FC<ComponentProps> = props => (
   <div className={props.className}>
-    <div className="header">
-      <div className="title">ハジメマシテ!!</div>
+    <div className="title">
+      <h1>ハジメマシテ!!</h1>
     </div>
     <div className="main">
+      <h3>About me</h3>
       <p>
         ネポタと申します。 性別は女です。
         <br />
-        <a href="https://twitter.com/soichiro_nitta" target="_blank">
+        <a
+          href="https://twitter.com/soichiro_nitta"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           旦那さん
         </a>
         と猫のプータローと一緒に毎日楽しく過ごしています。
         <br />
         よくツイッターにおりますので、気軽にリプください。
         <br />
+        <br />
+        <h3>SNS</h3>
+        <ul>
+          <li>
+            {' '}
+            <a
+              href="https://twitter.com/_nenenemo"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            {' '}
+            <a
+              href="https://www.instagram.com/_nenenemu/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Instagram
+            </a>
+          </li>{' '}
+          <li>
+            {' '}
+            <a
+              href="https://note.com/nepotaya"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              note
+            </a>
+          </li>{' '}
+          <li>
+            {' '}
+            <a
+              href="https://github.com/aya-nitta"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+        <br />
+        ご連絡は
+        <a
+          href="https://twitter.com/_nenenemo"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Twitter
+        </a>{' '}
+        かnepota99@gmail.comまでお願いします。
       </p>
     </div>
   </div>
--- src/pages/nepota/style.ts
+++ src/pages/nepota/style.ts
@@ -1,32 +1,34 @@
 import { css } from 'styled-components'
 
 export default css`
-  > .header {
+  line-height: 2;
+
+  > .title {
+    letter-spacing: 3px;
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    margin: 5rem auto;
+    width: 100%;
+    text-align: center;
+    font-size: 5rem;
+    font-weight: bold;
+    font-family: 'Mkpop';
+  }
+  > h1 {
     position: relative;
     width: 100%;
     height: 20rem;
-    /* background: #1c2c52; */
-    > .title {
-      display: flex;
-      justify-content: center;
-      position: absolute;
-      bottom: -5rem;
-      width: 100%;
-      text-align: center;
-      font-size: 5rem;
-      font-weight: bold;
-      font-family: 'Mkpop';
-    }
   }
   > .main {
     margin: auto 0;
-    padding: 30rem;
-    font-size: 2rem;
+    padding: 30rem 30rem;
+    font-size: 1.5rem;
     font-weight: bold;
     background-color: rgba(255, 255, 255, 0.6);
     border-radius: 5rem;
   }
   a {
-    color: #ffd632;
+    color: #888;
   }
 `
