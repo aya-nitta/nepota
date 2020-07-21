14d119c0f6afd71ae697b1c5d45f3a6f6596d8ef
soichiro-nitta <soic1226@gmail.com>
2020-07-21 19:13:09 +0900

    awaitを使用する

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,11 +2,15 @@ import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import style from './style'
 import { animations } from '@soichiro_nitta/animations'
+import { functions } from '~/utils/functions'
 
 type ContainerProps = {}
 type ComponentProps = {
   className: string
-  refs: { finger: React.MutableRefObject<HTMLImageElement | null> }
+  refs: {
+    finger: React.MutableRefObject<HTMLImageElement | null>
+    title: React.MutableRefObject<HTMLSpanElement | null>
+  }
 } & ContainerProps
 
 const Component: React.FC<ComponentProps> = props => (
@@ -14,7 +18,7 @@ const Component: React.FC<ComponentProps> = props => (
     <div className="wrapper">
       <a href="/nepota">
         <img src="/finger.svg" ref={props.refs.finger} />
-        <span>Nepota</span>
+        <span ref={props.refs.title}>Nepota</span>
       </a>
     </div>
   </div>
@@ -26,20 +30,22 @@ const StyledComponent = styled(Component)`
 
 const Container: React.FC<ContainerProps> = props => {
   const refs = {
-    finger: useRef<HTMLImageElement>(null)
+    finger: useRef<HTMLImageElement>(null),
+    title: useRef<HTMLSpanElement>(null)
   }
 
   useEffect(() => {
-    setInterval(() => {
+    setInterval(async () => {
       if (refs.finger.current) {
         animations.x(refs.finger.current, '5rem', 0.5, 'In')
         animations.scale(refs.finger.current, 1.2, 0.5, 'In')
-        setTimeout(() => {
-          if (refs.finger.current) {
-            animations.x(refs.finger.current, '0rem', 1, 'Out')
-            animations.scale(refs.finger.current, 1, 1, 'Out')
-          }
-        }, 500)
+
+        await functions.delay(0.5)
+
+        if (refs.finger.current) {
+          animations.x(refs.finger.current, '0rem', 1, 'Out')
+          animations.scale(refs.finger.current, 1, 1, 'Out')
+        }
       }
     }, 1500)
   }, [refs.finger])
